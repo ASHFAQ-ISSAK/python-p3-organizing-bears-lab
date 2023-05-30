6a270342e006b9ca36906ee1980b3e4c29ee18f0
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-05-30 14:48:42 +0300

    passed tests

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INTEGER,
+    sex VARCHAR(10),
+    color VARCHAR(50),
+    temperament VARCHAR(50),
+    alive BOOLEAN
+);
